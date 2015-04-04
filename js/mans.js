77cfb9e5d023bc5a5655b378d5e83a8848727cd6
Manuel Abrante <racerf5@gmail.com>
2015-04-03 21:04:35 -0430

    added moar info and graph

--- js/mans.js
+++ js/mans.js
@@ -15,23 +15,32 @@ Reveal.addEventListener( 'ready', function( event ) {
     if (event.fragment.id === 'bar-chart1'){
       barChart1();
     }
+    if (event.fragment.id === 'dos-chart'){
+      dosBars();
+    }
+    if (event.fragment.id === 'tres-chart'){
+      tresCharts();
+    }
+    if (event.fragment.id === 'four-chart'){
+      fourBars();
+    }
   });
 
   // IDC chart data
   var dataPie = [
     {
-        value: 74,
-        color:"rgba(247,70,74,1)",
-        fillColor : "rgba(247,70,74,0.5)",
-        highlight: "rgba(247,70,74,1)",
-        label: "Herramientas"
+      value: 76,
+      color:"rgba(247,70,74,1)",
+      fillColor : "rgba(247,70,74,0.5)",
+      highlight: "rgba(247,70,74,1)",
+      label: "Herramientas"
     },
     {
-        value: 20,
-        color: "rgba(70,191,189,1)",
-        fillColor: "rgba(70,191,189,0.5)",
-        highlight: "rgba(70,191,189,1)",
-        label: "Juegos",
+      value: 24,
+      color: "rgba(70,191,189,1)",
+      fillColor: "rgba(70,191,189,0.5)",
+      highlight: "rgba(70,191,189,1)",
+      label: "Juegos",
     },
   ];
 // Push IDC data into chart
@@ -56,14 +65,18 @@ Reveal.addEventListener( 'ready', function( event ) {
         color:"rgba(247,70,74,1)",
         fillColor : "rgba(247,70,74,0.5)",
         highlight: "rgba(247,70,74,1)",
-        label: "No"
+        label: "Si",
+        labelColor : 'white',
+        labelFontSize : '16'
     },
     {
         value: 58,
         color: "rgba(70,191,189,1)",
         fillColor: "rgba(70,191,189,0.5)",
         highlight: "rgba(70,191,189,1)",
-        label: "Si",
+        label: "No",
+        labelColor : 'white',
+        labelFontSize : '16'
     },
   ];
   // fPush data to Unsatisfaction chart
@@ -76,7 +89,7 @@ Reveal.addEventListener( 'ready', function( event ) {
       animateRotate : true,
      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%=': '%><%=segments[i].value%><%='%'%><%}%></li><%}%></ul>"
     });
-    document.querySelector('#problem td:last-child').innerHTML = unChart.generateLegend();
+    document.querySelector('#item1 td:last-child').innerHTML = unChart.generateLegend();
   }
 
   // Bar Data chart
@@ -112,7 +125,114 @@ Reveal.addEventListener( 'ready', function( event ) {
     });
     document.querySelector('#interr #legend-container').innerHTML = dataChart.generateLegend();
   }
-  
+   // Bar Data chart
+  var dosBar = {
+    labels: ['Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    datasets: [
+    {
+      fillColor: 'rgba(70,191,189,1)',
+      strokeColor: 'rgba(70,191,189,1)',
+      pointColor: '#165683',
+      pointStrokeColor: '#000',
+      pointHighlightFill: '#000',
+      pointHighlightStroke: 'rgba(31,119,180,)',
+      data: [96,98,87,67]
+    },
+    {
+      fillColor: 'rgba(247,70,74,1)',
+      strokeColor: 'rgba(247,70,74,1)',
+      pointColor: '#165683',
+      pointStrokeColor: '#000',
+      pointHighlightFill: '#000',
+      pointHighlightStroke: 'rgba(31,119,180,)',
+      data: [4,2,13,33]
+    }
+    ]
+  };
 
-  
+  //push data in line chart # 1
+  function dosBars() {
+    var barChart1 = document.querySelector('#dos-chart').getContext('2d');
+    var dataChart = new Chart(barChart1).Bar(dosBar, {
+      scaleBeginAtZero : true,
+      scaleShowGridLines : true,
+      scaleGridLineWidth : 1,
+      scaleShowHorizontalLines: true,
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i]%><%}%></li><%}%></ul>"
+
+    });
+    document.querySelector('#item3-6 #legend-container').innerHTML = dataChart.generateLegend();
+  } 
+
+  var dataTresChart = [
+    {
+        value: 85,
+        color:"rgba(247,70,74,1)",
+        fillColor : "rgba(247,70,74,0.5)",
+        highlight: "rgba(247,70,74,1)",
+        label: "Si",
+        labelColor : 'white',
+        labelFontSize : '16'
+    },
+    {
+        value: 15,
+        color: "rgba(70,191,189,1)",
+        fillColor: "rgba(70,191,189,0.5)",
+        highlight: "rgba(70,191,189,1)",
+        label: "No",
+        labelColor : 'white',
+        labelFontSize : '16'
+    },
+  ];
+  function tresCharts(){
+    var unCtx = document.querySelector('#tres-chart').getContext('2d');
+    var unChart = new Chart(unCtx).Pie(dataTresChart, {
+      segmentShowStroke : true,
+      animationSteps : 100,
+      animationEasing : "easeOutBounce",
+      animateRotate : true,
+     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%=': '%><%=segments[i].value%><%='%'%><%}%></li><%}%></ul>"
+    });
+    document.querySelector('#item7 td:last-child').innerHTML = unChart.generateLegend();
+  }
+
+  var functionColores = function(){
+    var colores = ['orange', 'red', 'green'];
+    for (var i = 0; i <= colores.length; i++){
+      return colores[i];
+    }
+  };
+  var randomColorGeneator = function () { 
+    return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+};
+ console.log(functionColores());
+  var fourBar = {
+    labels: ['Notas Evaluaciones', 'Pre-Inscripcion Asignaturas', 'Mi Horario', 'Notas de Evaluaciones'],
+    datasets: [
+    {
+      fillColor: randomColorGeneator(),
+      data: [96,98,87,67]
+    }
+    ]
+  };
+
+   /* fourBar.datasets[0].bars[0].fillColor = "green"; //bar 1
+    fourBar.datasets[0].bars[1].fillColor = "orange"; //bar 2
+    fourBar.datasets[0].bars[2].fillColor = "red"; //bar 3
+    fourBar.update()*/
+    //console.log(fourBar);
+
+  //push data in line chart # 1
+ function fourBars() {
+    var barChart1 = document.querySelector('#four-chart').getContext('2d');
+    var dataChart = new Chart(barChart1).Bar(fourBar, {
+      scaleBeginAtZero : true,
+      scaleShowGridLines : true,
+      scaleGridLineWidth : 1,
+      scaleShowHorizontalLines: true,
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i]%><%}%></li><%}%></ul>"
+
+    });
+    document.querySelector('#item8 #legend-container').innerHTML = dataChart.generateLegend();
+  }
 });
